b134c4e00d4bc91eb43ce14ca2c242f6f18e8ffa
Shane Riegodedios <shaner261996@gmail.com>
2016-07-27 19:47:03 -0500

    Calculator

---
+++ Labs/Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.answerBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // answerBox
+            // 
+            this.answerBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerBox.Location = new System.Drawing.Point(115, 36);
+            this.answerBox.Name = "answerBox";
+            this.answerBox.ReadOnly = true;
+            this.answerBox.Size = new System.Drawing.Size(264, 38);
+            this.answerBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 88);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 88);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(328, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 88);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(84, 239);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 88);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(207, 239);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(92, 88);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(328, 239);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(92, 88);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(84, 349);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(92, 88);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(207, 349);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(92, 88);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(328, 349);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(92, 88);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(207, 460);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(213, 88);
+            this.clearButton.TabIndex = 11;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(84, 460);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(92, 88);
+            this.button0.TabIndex = 12;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 590);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.answerBox);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox answerBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button button0;
+    }
+}
+
---
+++ Labs/Calculator/Calculator/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        private Button[] buttons;
+        private int count = 0; 
+
+       
+        public Calculator()
+        {
+            
+            InitializeComponent();
+            
+            buttons = new Button[10];
+            buttons[0] = button0;
+            buttons[1] = button1;
+            buttons[2] = button2;
+            buttons[3] = button3;
+            buttons[4] = button4;
+            buttons[5] = button5;
+            buttons[6] = button6;
+            buttons[7] = button7;
+            buttons[8] = button8;
+            buttons[9] = button9;
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Click += new EventHandler(buttonClick);
+
+
+            }
+
+            clearButton.Click += new EventHandler(clearBox);
+
+        }
+
+
+        private void boxFull()
+        {
+            if(count == 16)
+            {
+                for (int i = 0; i < buttons.Length; i++)
+                {
+                    buttons[i].Enabled = false;
+
+
+                }
+                
+
+            }
+
+           // if(answerBox.i)
+
+
+        }
+
+
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            boxFull();
+            Button b = (Button)sender;
+            answerBox.Text += b.Text;
+            count++;
+
+        }
+
+        private void clearBox(object sender, EventArgs e)
+        {
+            answerBox.Text = "";
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Enabled = true;
+
+
+            }
+            count = 0;
+
+        }
+
+       
+
+
+       
+
+        }
+    }
+
